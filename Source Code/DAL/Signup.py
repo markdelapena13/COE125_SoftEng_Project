73eab781cd75ac826bbf9e0d436e1efa75540afc
Mark Dela Peña <42404835+markdelapena13@users.noreply.github.com>
2018-09-26 15:26:40 +0900

    Update Signup.py

--- Source Code/DAL/Signup.py
+++ Source Code/DAL/Signup.py
@@ -7,8 +7,28 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from registerSucc import Succ
+
 
 class Ui_Dialog(object):
+
+
+    def registerSucc(self):
+        self.window=QtWidgets.QDialog()
+        self.succ = Succ()
+        self.succ.setupUi(self.window)
+        self.window.show()
+     
+    def registerUser(self):
+        users = open("Credentials.txt","a")
+        users.write(self.UserEnter.text())
+        users.write(",")
+        users.write(self.PassEnter.text())
+        users.write("\n")
+        users.close()
+
+
+    
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
         Dialog.resize(614, 378)
@@ -24,15 +44,32 @@ class Ui_Dialog(object):
         self.PassLabel = QtWidgets.QLabel(Dialog)
         self.PassLabel.setGeometry(QtCore.QRect(150, 140, 131, 41))
         self.PassLabel.setObjectName("PassLabel")
+
+
+        
+        ####### Field to Enter Username #########
         self.UserEnter = QtWidgets.QLineEdit(Dialog)
         self.UserEnter.setGeometry(QtCore.QRect(150, 80, 271, 31))
         self.UserEnter.setObjectName("UserEnter")
+
+        ######## Field to Enter Password #######
+                                            
         self.PassEnter = QtWidgets.QLineEdit(Dialog)
         self.PassEnter.setGeometry(QtCore.QRect(150, 190, 281, 31))
         self.PassEnter.setObjectName("PassEnter")
-        self.pushButton = QtWidgets.QPushButton(Dialog)
-        self.pushButton.setGeometry(QtCore.QRect(230, 280, 121, 41))
-        self.pushButton.setObjectName("pushButton")
+
+
+
+        ####### button to register #########
+        self.pushReg = QtWidgets.QPushButton(Dialog)
+        self.pushReg.setGeometry(QtCore.QRect(230, 280, 121, 41))
+        self.pushReg.setObjectName("pushReg")
+        self.pushReg.clicked.connect(self.registerUser)
+        self.pushReg.clicked.connect(self.registerSucc)
+        
+        
+
+        
 
         self.retranslateUi(Dialog)
         self.buttonBox.accepted.connect(Dialog.accept)
@@ -44,7 +81,11 @@ class Ui_Dialog(object):
         Dialog.setWindowTitle(_translate("Dialog", "User Registration"))
         self.UserLabel.setText(_translate("Dialog", "<html><head/><body><p><span style=\" font-size:10pt;\">Desired Username:</span></p></body></html>"))
         self.PassLabel.setText(_translate("Dialog", "<html><head/><body><p><span style=\" font-size:10pt;\">Password:</span></p></body></html>"))
-        self.pushButton.setText(_translate("Dialog", "Register"))
+        self.pushReg.setText(_translate("Dialog", "Register"))
+
+
+        
+        
 
 
 if __name__ == "__main__":
